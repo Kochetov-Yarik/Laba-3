25275f35a3dccea06d367e09a7f51f99b5ca509a
Kochetov-Yarik <bogbana9@gmail.com>
2026-03-22 21:04:46 +0200

    Update Task.cs
    Створення Main() для тестування роботи

--- Task.cs
+++ Task.cs
@@ -38,3 +38,34 @@
         return $"Платіж №{_paymentId} | Тип: {_type} | Сума: {_amount} грн.";
     }
 }
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("--- Створення платежів ---");
+        Payment payment1 = new Payment(1686.70m, "Оплата гуртожитку");
+        Payment payment2 = new Payment("Переказ за карту");
+
+        Console.WriteLine(payment1.ToString());
+        Console.WriteLine(payment2.ToString());
+
+        Console.WriteLine("\n--- Зміна суми ---");
+
+        payment2.ChangeAmount(400.40m);
+        Console.WriteLine("Після поповнення: " + payment2.ToString());
+
+        Console.Write("\nСпроба встановити -50 грн: \n");
+        payment2.ChangeAmount(-50m);
+
+        Console.WriteLine("\n--- Перевірка чи великий платіж ---");
+
+        decimal limit = 1000m;
+        Console.WriteLine($"Чи {payment1.ToString()} більший за {limit}? {payment1.IsLarge(limit)}");
+        Console.WriteLine($"Чи {payment2.ToString()} більший за {limit}? {payment2.IsLarge(limit)}");
+
+        Console.WriteLine($"\nВсього в системі створено платежів: {Payment.TotalPayments()}");
+
+        Console.ReadLine();
+    }
+}
