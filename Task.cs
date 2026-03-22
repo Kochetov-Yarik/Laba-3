4cf21e24ae4a68921b84c43a2940d6d4df5d0296
Kochetov-Yarik <bogbana9@gmail.com>
2026-03-22 20:22:22 +0200

    Update Task.cs
    Створення платежу з сумою і без неї

--- Task.cs
+++ Task.cs
@@ -4,5 +4,14 @@
     private decimal _amount;
     private string _type;
     private static int _totalPayments;
-
+    public Payment(decimal amount, string type)
+    {
+        _totalPayments++;
+        _paymentId = _totalPayments;
+        _type = type;
+        _amount = amount;
+    }
+    public Payment(string type) : this(0m, type)
+    {
+    }
 }
